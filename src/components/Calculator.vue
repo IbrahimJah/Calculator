5b0607eef6ae65e77d1042fbbe6e8cc0546857a4
Ibrahim Jah <ibrahimjah@hotmail.co.uk>
2022-01-18 09:20:50 +0000

    Calculator2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -6,20 +6,13 @@
 
     <div @click="percent" class="percent button">%</div>
     <div @click="clear" class="clear button">Clear</div>
-    <div @click="append('7')" class="button">7</div>
-    <div @click="append('8')" class="button">8</div>
-    <div @click="append('9')" class="button">9</div>
+    <div @click="append(n)" class="button" v-for="n in keys" :key="n" >{{n}}</div>
+   
+    
     <div @click="multiply" class="button">x</div>
-    <div @click="append('4')" class="button">4</div>
-    <div @click="append('5')" class="button">5</div>
-    <div @click="append('6')" class="button">6</div>
     <div @click="minus" class="button">-</div>
-    <div @click="append('1')" class="button">1</div>
-    <div @click="append('2')" class="button">2</div>
-    <div @click="append('3')" class="button">3</div>
     <div @click="plus" class="button">+</div>
     <div @click="divide" class="button">/</div>
-    <div @click="append('0')" class="button">0</div>
     <div @click="dot" class="button">.</div>
     <div @click="equal" class="button">=</div>
   </div>
@@ -34,27 +27,12 @@ export default {
       response: "",
       operator: null,
       operatorClicked: false,
+      keys: [7,8,9,4,5,6,1,2,3,0],
     };
   },
-
   //button function and logic
   methods: {
-    clear() {
-      this.response = '';
-    },
-
-    percent() {
-      this.response = `${parseFloat(this.response) / 100}`; //parseFloat allows stings to be converted into a floating point, ${} is a template literal that lets me insert variables/ expressions
-    },
-
-    dot() {
-      //the if statement will not add another dot if there is one already
-      if (this.response.indexOf(".") === -1) {
-        this.append(".");
-      }
-    },
-
-    //a method to add to append string
+ //a method to add to append string
     append(number) {
       //empty display if we clicked on an operator
       if (this.operatorClicked) {//if true then clear else set to false
@@ -65,31 +43,39 @@ export default {
       this.response = this.response + number;
     },
 
+    clear() {
+      this.response = '';
+    },
+    percent() {
+      this.response = `${parseFloat(this.response) / 100}`; //parseFloat allows stings to be converted into a floating point, ${} is a template literal that lets me insert variables/ expressions
+    },
+    dot() {
+      //the if statement will not add another dot if there is one already
+      if (this.response.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+   
     divide() {
       this.operator = (a, b) => a / b;
       this.setPrevious();
     },
-
     multiply() {
       this.operator = (a, b) => a * b;
       this.setPrevious();
     },
-
     minus() {
       this.operator = (a, b) => a - b;
       this.setPrevious();
     },
-
     plus() {
       this.operator = (a, b) => a + b;
       this.setPrevious();
     },
-
     setPrevious() {
       this.operatorClicked = true;
       this.previous = this.response;
     },
-
     equal() {
       //takes the current value and runs it against the previous
       this.response = `${this.operator(
